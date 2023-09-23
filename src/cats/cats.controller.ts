78bce8ce7d3b5bcd5bb105d4976475b5448476cb
David Chavez <depch47@gmail.com>
2023-09-22 23:03:12 -0500

    Following docs guards

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -9,16 +9,21 @@ import {
   HttpException,
   HttpStatus,
   ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { RolesGuard } from 'src/common/auth/roles.guard';
+import { Roles } from 'src/common/auth/roles.decorator';
 
 @Controller('/cats')
+@UseGuards(RolesGuard)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
+  @Roles(["admin"])
   create(@Body() createCatDto: CreateCatDto) {
     return this.catsService.create(createCatDto);
   }
